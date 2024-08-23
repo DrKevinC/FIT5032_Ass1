d5eafca749c0b1fb7eabda6bbc45c672fd3ffce1
Kevin Cody <kcod0001@student.monash.edu>
2024-08-23 20:39:14 +1000

    Initial project creation - router created - Initial router views created - Basic Header links created

---
+++ Fit5032Assignment/src/router/index.js
@@ -0,0 +1,77 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ResourcesView from '@/views/ResourcesView.vue'
+import EventView from '@/views/EventView.vue'
+import EventOverview from '@/views/EventOverview.vue'
+import EventManager from '@/views/EventManager.vue'
+import EventCreator from '@/views/EventCreator.vue'
+import DonateView from '@/views/DonateView.vue'
+import DiscussionView from '@/views/DiscussionView.vue'
+import DiscussionOverview from '@/views/DiscussionOverview.vue'
+import ContactView from '@/views/ContactView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  },
+  {
+    path: '/resources',
+    name: 'Resources',
+    component: ResourcesView
+  },
+  {
+    path: '/event',
+    name: 'Event',
+    component: EventView
+  },
+  {
+    path: '/eventOverview',
+    name: 'EventOverview',
+    component: EventOverview
+  },
+  {
+    path: '/eventManager',
+    name: 'EventManager',
+    component: EventManager
+  },
+  {
+    path: '/eventCreator',
+    name: 'EventCreator',
+    component: EventCreator
+  },
+  {
+    path: '/donate',
+    name: 'Donate',
+    component: DonateView
+  },
+  {
+    path: '/discussion',
+    name: 'Discussion',
+    component: DiscussionView
+  },
+  {
+    path: '/discussionOverview',
+    name: 'DiscussionOverview',
+    component: DiscussionOverview
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactView
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ Fit5032Assignment/src/views/DonateView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="about">
+        <h1>Donate View</h1>
+        <p>This is a subheading</p>
+    </div>
+</template>
+
+<script setup>
+// No script needed for now
+</script>
+
+<style>
+</style>
